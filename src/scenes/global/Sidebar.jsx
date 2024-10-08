c8c0bc3849f45f46f270cdc8dce9f958ef8c70b3
Prateek Singh <prateeksingh@Prateeks-MacBook-Air.local>
2024-10-08 20:36:00 +0530

    Profile photo part in SideBar updated

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,274 @@
+// import { useState } from "react";
+// import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+// import { Box, IconButton, Typography, useTheme } from "@mui/material";
+// import { Link } from "react-router-dom";
+// import "react-pro-sidebar/dist/css/styles.css";
+// import { tokens } from "../../theme";
+// import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+// import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+// import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+// import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+// import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+// import CategoryIcon from "@mui/icons-material/Category";
+// import LoyaltyIcon from "@mui/icons-material/Loyalty";
+// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+// import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+// import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+// import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+// const Item = ({ title, to, icon, selected, setSelected }) => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   return (
+//     <MenuItem
+//       active={selected === title}
+//       style={{
+//         color: colors.grey[100],
+//       }}
+//       onClick={() => setSelected(title)}
+//       icon={icon}
+//     >
+//       <Typography>{title}</Typography>
+//       <Link to={to} />
+//     </MenuItem>
+//   );
+// };
+
+// const Sidebar = () => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   const [isCollapsed, setIsCollapsed] = useState(false);
+//   const [selected, setSelected] = useState("Dashboard");
+
+//   return (
+//     <Box
+//       sx={{
+//         "& .pro-sidebar-inner": {
+//           background: `${colors.primary[400]} !important`,
+//         },
+//         "& .pro-icon-wrapper": {
+//           backgroundColor: "transparent !important",
+//         },
+//         "& .pro-inner-item": {
+//           padding: "5px 35px 5px 20px !important",
+//         },
+//         "& .pro-inner-item:hover": {
+//           color: "#868dfb !important",
+//         },
+//         "& .pro-menu-item.active": {
+//           color: "#6870fa !important",
+//         },
+//         height: "100vh",
+//         position: "sticky",
+//         top: 0,
+//         overflowY: "auto",
+//       }}
+//     >
+//       <ProSidebar collapsed={isCollapsed} width="235px">
+//         <Menu iconShape="square">
+//           {/* LOGO AND MENU ICON */}
+//           <MenuItem
+//             onClick={() => setIsCollapsed(!isCollapsed)}
+//             icon={
+//               isCollapsed ? (
+//                 <img
+//                   alt="profile-user"
+//                   width="40px"
+//                   height="40px"
+//                   className="border-radius:50%"
+//                   src={`../../assets/user.jpg`}
+//                   style={{ cursor: "pointer", borderRadius: "20%" }}
+//                 />
+//               ) : (
+//                 <img
+//                   alt="profile-user"
+//                   width="40px"
+//                   height="40px"
+//                   src={`../../assets/user.jpg`}
+//                   style={{ cursor: "pointer", borderRadius: "20%" }}
+//                 />
+//               )
+//             }
+//             style={{
+//               margin: "10px 0 20px 0",
+//               color: colors.grey[100],
+//             }}
+//           >
+//             {!isCollapsed && (
+//               <Box
+//                 display="flex"
+//                 justifyContent="space-between"
+//                 alignItems="center"
+//                 ml="15px"
+//               >
+//                 <Typography variant="h3" color={colors.grey[100]}>
+//                   DashBoard
+//                 </Typography>
+//                 {/* <Box display="flex" justifyContent="center" alignItems="center">
+//                 <img
+//                   alt="profile-user"
+//                   width="20px"
+//                   height="20px"
+//                   src={`../../assets/user.jpg`}
+//                   style={{ cursor: "pointer", borderRadius: "0%" }}
+//                 />
+//               </Box> */}
+//                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+//                   <MenuOutlinedIcon />
+//                 </IconButton>
+//               </Box>
+//             )}
+//           </MenuItem>
+
+//           {!isCollapsed && (
+//             <Box mb="25px">
+//               <Box display="flex" justifyContent="center" alignItems="center">
+//                 <img
+//                   alt="profile-user"
+//                   width="50px"
+//                   height="50px"
+//                   src={`../../assets/user.jpg`}
+//                   style={{ cursor: "pointer", borderRadius: "20%" }}
+//                 />
+//               </Box>
+//               <Box textAlign="center">
+//                 <Typography
+//                   variant="h2"
+//                   color={colors.grey[100]}
+//                   fontWeight="bold"
+//                   sx={{ m: "10px 0 0 0" }}
+//                 >
+//                   Sweata
+//                 </Typography>
+//                 <Typography variant="h5" color={colors.greenAccent[500]}>
+//                   Developer
+//                 </Typography>
+//               </Box>
+//             </Box>
+//           )}
+
+//           <Box paddingLeft={isCollapsed ? undefined : "2.5px"}>
+//             <Item
+//               title="Dashboard"
+//               to="/"
+//               icon={<HomeOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+
+//             <Typography
+//               variant="h6"
+//               color={colors.grey[300]}
+//               sx={{ m: "15px 0 5px 20px" }}
+//             >
+//               Data
+//             </Typography>
+//             <Item
+//               title="Manage Users"
+//               to="/team"
+//               icon={<PeopleOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             <Item
+//               title="Manage Customers"
+//               to="/contacts"
+//               icon={<ContactsOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             {/* <Item
+//               title="Manage Subscription"
+//               to="/contacts"
+//               icon={<LoyaltyIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             /> */}
+//             <Item
+//               title="Invoices Balances"
+//               to="/invoices"
+//               icon={<ReceiptOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+
+//             <Typography
+//               variant="h6"
+//               color={colors.grey[300]}
+//               sx={{ m: "15px 0 5px 20px" }}
+//             >
+//               Pages
+//             </Typography>
+//             <Item
+//               title="Profile Form"
+//               to="/form"
+//               icon={<PersonOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             <Item
+//               title="Calendar"
+//               to="/calendar"
+//               icon={<CalendarTodayOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             <Item
+//               title="FAQ Page"
+//               to="/faq"
+//               icon={<HelpOutlineOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+
+//             <Typography
+//               variant="h6"
+//               color={colors.grey[300]}
+//               sx={{ m: "15px 0 5px 20px" }}
+//             >
+//               Charts
+//             </Typography>
+//             <Item
+//               title="Bar Chart"
+//               to="/bar"
+//               icon={<BarChartOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             <Item
+//               title="Pie Chart"
+//               to="/pie"
+//               icon={<PieChartOutlineOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             <Item
+//               title="Line Chart"
+//               to="/line"
+//               icon={<TimelineOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//             <Item
+//               title="Geography Chart"
+//               to="/geography"
+//               icon={<MapOutlinedIcon />}
+//               selected={selected}
+//               setSelected={setSelected}
+//             />
+//           </Box>
+//         </Menu>
+//       </ProSidebar>
+//     </Box>
+//   );
+// };
+
+// export default Sidebar;
+
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
@@ -19,6 +290,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { green } from "@mui/material/colors";
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
@@ -70,27 +342,22 @@ const Sidebar = () => {
     >
       <ProSidebar collapsed={isCollapsed} width="235px">
         <Menu iconShape="square">
-          {/* LOGO AND MENU ICON */}
           <MenuItem
             onClick={() => setIsCollapsed(!isCollapsed)}
             icon={
-              isCollapsed ? (
-                <img
-                  alt="profile-user"
-                  width="40px"
-                  height="40px"
-                  src={`../../assets/user.jpg`}
-                  style={{ cursor: "pointer", borderRadius: "20%" }}
-                />
-              ) : (
-                <img
-                  alt="profile-user"
-                  width="40px"
-                  height="40px"
-                  src={`../../assets/user.jpg`}
-                  style={{ cursor: "pointer", borderRadius: "20%" }}
-                />
-              )
+              <img
+                alt="profile-user"
+                width={isCollapsed ? "40px" : "50px"}
+                height={isCollapsed ? "40px" : "50px"}
+                src={`../../assets/user.png`}
+                style={{
+                  height: "39px",
+                  width: "40px",
+                  borderRadius: "50%",
+                  cursor: "pointer",
+                  border: "1px solid white",
+                }}
+              />
             }
             style={{
               margin: "10px 0 20px 0",
@@ -107,15 +374,6 @@ const Sidebar = () => {
                 <Typography variant="h3" color={colors.grey[100]}>
                   DashBoard
                 </Typography>
-                {/* <Box display="flex" justifyContent="center" alignItems="center">
-                <img
-                  alt="profile-user"
-                  width="20px"
-                  height="20px"
-                  src={`../../assets/user.jpg`}
-                  style={{ cursor: "pointer", borderRadius: "0%" }}
-                />
-              </Box> */}
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
                 </IconButton>
@@ -130,8 +388,13 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="50px"
                   height="50px"
-                  src={`../../assets/user.jpg`}
-                  style={{ cursor: "pointer", borderRadius: "20%" }}
+                  src={`../../assets/user.png`}
+                  style={{
+                    cursor: "pointer",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    border: "2px solid white",
+                  }}
                 />
               </Box>
               <Box textAlign="center">
@@ -158,7 +421,6 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-
             <Typography
               variant="h6"
               color={colors.grey[300]}
@@ -180,13 +442,6 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-            {/* <Item
-              title="Manage Subscription"
-              to="/contacts"
-              icon={<LoyaltyIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            /> */}
             <Item
               title="Invoices Balances"
               to="/invoices"
@@ -194,7 +449,6 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-
             <Typography
               variant="h6"
               color={colors.grey[300]}
@@ -223,7 +477,6 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-
             <Typography
               variant="h6"
               color={colors.grey[300]}
